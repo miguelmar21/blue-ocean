6f596fb9a6d16eb4daa8c840a0c0a368b028ce69
RasNewton <princegyekyeaddai@gmail.com>
2021-12-08 14:45:32 -0600

    gitingore env

--- server/readme.js
+++ server/readme.js
@@ -1,1 +1,5 @@
 // create your config.env file in this root folder
+
+// PORT = 3000
+// MONGO_URI = "mongodb+srv://spaghetti-db:Password123@spaghetti-cluster.p5ovz.mongodb.net/myFirstDatabase"
+// NODE_ENV = development
