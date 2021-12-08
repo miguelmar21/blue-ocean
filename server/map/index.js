d5623544031d0b501b3844bc497bbd63dd06bfbe
RasNewton <princegyekyeaddai@gmail.com>
2021-12-08 13:25:05 -0600

    data

--- server/map/index.js
+++
@@ -1,8 +0,0 @@
-var routes = require('express').Router();
-
-routes.get('/', (req, res) => {
-  console.log('get request received!')
-  res.status(200).send('I have received your get request!')
-});
-
-module.exports = routes;
--- server/server.js
+++ server/server.js
@@ -1,22 +1,43 @@
-require('dotenv').config()
-var cors = require('cors');
-var path = require('path');
-const express = require("express");
-const db = require('../database')
-//ATLAS FOR SHARED DB
 
+//environment
+const path = require('path');
+require("dotenv").config({ path: path.join(__dirname, "./config.env") });
+
+//server
+const express = require('express');
+const { urlencoded } = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
 const app = express();
-app.use(express.static(__dirname + '/../dist/'));
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;//deployment
 
-app.use(express.json());
+// middleware
+app.use(morgan('dev'));
 app.use(cors());
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../dist')));
 
-var map = require('./map');
-// var map = require('./profiles');
+// mongo connection
+const mongoose = require('mongoose');
 
-app.use('/map', map);
+const url = process.env.MONGO_URI;//deployment
+const config = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
+// const connect = mongoose.connect(url, config);//deployment
 
-app.listen(PORT, ()=> {
-  console.log(`listening on port ${PORT}`)
-});
+const localUrl = 'mongodb://localhost:27017/blueOcean';
+// testing
+const connect = mongoose.connect(localUrl);
+
+connect
+  .then(db => console.log('connected to DB'))
+  .catch(err => console.error(err));
+
+
+//routes
+// import your routes below here
+
+app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
