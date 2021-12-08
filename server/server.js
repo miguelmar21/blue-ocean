90833a19dd591016627d076336d2a026a14deed8
Jeff DeSouza <jeffdesouzadev@gmail.com>
2021-12-08 11:15:17 -0600

    mongo hooked up

--- server/server.js
+++ server/server.js
@@ -8,12 +8,25 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(express.static(__dirname + '/../dist/'));
 const PORT = process.env.PORT || 3000;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
 app.use(express.json());
 app.use(cors());
 
 var map = require('./map');
+// var map = require('./profiles');
 
 app.use('/map', map);
+
+const endpoint = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@spaghetti-cluster.p5ovz.mongodb.net/myFirstDatabase`
+
+mongoose.connect(endpoint)
+const db = mongoose.connection
+
+db.once('open', () => {
+  console.log('connected!')
+})
 
 
 
