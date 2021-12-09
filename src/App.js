545fda528bcaa9ebc9f126026a98b15422269251
Miguel Regalado <miguelmobil21@gmail.com>
2021-12-09 14:44:03 -0600

    Added onClick feature on map

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
 import axios from "axios";
-import Map from './Map/Map';
+import Map from "./Map/Map";
 
 function App() {
   return (
-    <Map
-      isMarkerShown
-      googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBMyy9-eKn7PhXppZ9MnlKQrgXlDDdV6o0&v=3.exp&libraries=geometry,drawing,places"
-      loadingElement={<div style={{ height: `100%` }} />}
-      containerElement={<div style={{ height: `400px` }} />}
-      mapElement={<div style={{ height: `100%` }} />}
-    />
+    <div>
+      <Map
+        isMarkerShown
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBMyy9-eKn7PhXppZ9MnlKQrgXlDDdV6o0&v=3.exp&libraries=geometry,drawing,places"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `400px` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+      <div className='hi'>
+        <h2>Hi</h2>
+      </div>
+    </div>
   );
 }
 
--- src/Map/Map.js
+++ src/Map/Map.js
@@ -4,8 +4,21 @@ import {
   withGoogleMap,
   GoogleMap,
   Marker,
+  InfoWindow
 } from "react-google-maps";
+import usePlacesAutocomplete, {
+  getGeoCode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from '@reach/combobox'
 import mapStyle from "./mapStyle";
+
 
 
 const options = {
@@ -17,23 +30,55 @@ const options = {
 const Map = withScriptjs(
   withGoogleMap((props) => {
     const [markers, setMarkers] = useState([]);
+    const [selected, setSelected] = React.useState(null);
+
+    const onMapClick = React.useCallback((event) => {
+      setMarkers(currentMarkers => [
+        ...currentMarkers,
+        {
+          lat: event.latLng.lat(),
+          lng: event.latLng.lng(),
+          time: new Date
+        }
+      ])
+    }, []);
+
+    const mapRef = React.useRef();
+    const onMapLoad = React.useCallback((map) => {
+      mapRef.current = map;
+    }, [])
 
     return (
       <GoogleMap
         defaultZoom={8}
         defaultCenter={{ lat: 27.522628, lng: -99.489061 }}
         options={options}
-        onClick={(event) => {
-          setMarkers([...markers, {
-            lat: event.latLng.lat(),
-            lng: event.latLng.lng(),
-            time: new Date
-          }])
-        }}
+        onClick={onMapClick}
+        onLoad={onMapLoad}
       >
-        {props.isMarkerShown && (
-          <Marker position={{ lat: 27.522628, lng: -99.489061 }} />
-        )}
+        {markers.map(marker => 
+          <Marker 
+            key={marker.time.toISOString()}
+            position={{ lat: marker.lat, lng: marker.lng }} 
+            icon={{
+              url: 'https://svg-clipart.com/svg/color/oLsCLwr-blue-musical-note-vector.svg',
+              scaledSize: new window.google.maps.Size(30, 30),
+              origin: new window.google.maps.Point(0, 0),
+              anchor: new window.google.maps.Point(15, 15)
+            }}
+            onClick={() => 
+              setSelected(marker)}/>
+          )}
+          {selected ? 
+          <InfoWindow 
+            position={{lat: selected.lat, lng: selected.lng}}
+            onCloseClick={() => {
+              setSelected(null);
+            }}>
+            <div>
+              <h2>Music performance here!</h2>
+            </div>
+          </InfoWindow> : null}
       </GoogleMap>
     );
   })
