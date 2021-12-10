208581b879a50cb2c68d7c82e227fdc6572c6715
Miguel Regalado <miguelmobil21@gmail.com>
2021-12-09 20:44:25 -0600

    Search component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from "react";
 import axios from "axios";
 import Map from "./Map/Map";
+import API_KEY from '../config'
 
 function App() {
   return (
     <div>
       <Map
         isMarkerShown
-        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBMyy9-eKn7PhXppZ9MnlKQrgXlDDdV6o0&v=3.exp&libraries=geometry,drawing,places"
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `400px` }} />}
         mapElement={<div style={{ height: `100%` }} />}
       />
-      <div className='hi'>
-        <h2>Hi</h2>
-      </div>
     </div>
   );
 }
--- src/Map/Map.js
+++ src/Map/Map.js
@@ -7,7 +7,7 @@ import {
   InfoWindow,
 } from "react-google-maps";
 import usePlacesAutocomplete, {
-  getGeoCode,
+  getGeocode,
   getLatLng,
 } from "use-places-autocomplete";
 import {
@@ -47,39 +47,42 @@ const Map = withScriptjs(
     }, []);
 
     return (
-      <GoogleMap
-        defaultZoom={8}
-        defaultCenter={{ lat: 27.522628, lng: -99.489061 }}
-        options={options}
-        onClick={onMapClick}
-        onLoad={onMapLoad}
-      >
-        {markers.map((marker) => (
-          <Marker
-            key={marker.time.toISOString()}
-            position={{ lat: marker.lat, lng: marker.lng }}
-            icon={{
-              url: "https://svg-clipart.com/svg/color/oLsCLwr-blue-musical-note-vector.svg",
-              scaledSize: new window.google.maps.Size(30, 30),
-              origin: new window.google.maps.Point(0, 0),
-              anchor: new window.google.maps.Point(15, 15),
-            }}
-            onClick={() => setSelected(marker)}
-          />
-        ))}
-        {selected ? (
-          <InfoWindow
-            position={{ lat: selected.lat, lng: selected.lng }}
-            onCloseClick={() => {
-              setSelected(null);
-            }}
-          >
-            <div>
-              <h2>Music performance here!</h2>
-            </div>
-          </InfoWindow>
-        ) : null}
-      </GoogleMap>
+      <div>
+        <Search />
+        <GoogleMap
+          defaultZoom={8}
+          defaultCenter={{ lat: 27.522628, lng: -99.489061 }}
+          options={options}
+          onClick={onMapClick}
+          onLoad={onMapLoad}
+        >
+          {markers.map((marker) => (
+            <Marker
+              key={marker.time.toISOString()}
+              position={{ lat: marker.lat, lng: marker.lng }}
+              icon={{
+                url: "https://svg-clipart.com/svg/color/oLsCLwr-blue-musical-note-vector.svg",
+                scaledSize: new window.google.maps.Size(30, 30),
+                origin: new window.google.maps.Point(0, 0),
+                anchor: new window.google.maps.Point(15, 15),
+              }}
+              onClick={() => setSelected(marker)}
+            />
+          ))}
+          {selected ? (
+            <InfoWindow
+              position={{ lat: selected.lat, lng: selected.lng }}
+              onCloseClick={() => {
+                setSelected(null);
+              }}
+            >
+              <div>
+                <h2>Music performance here!</h2>
+              </div>
+            </InfoWindow>
+          ) : null}
+        </GoogleMap>
+      </div>
     );
   })
 );
@@ -99,6 +102,36 @@ function Search() {
       radius: 200 * 1000,
     },
   });
+
+  return (
+    <Combobox
+      onSelect={(address) => {
+        getGeocode({ address })
+          .then((geocode) => {
+            return getLatLng(geocode[0]);
+          })
+          .then((LatLng) => {
+            const { lat, lng } = LatLng;
+          })
+          .catch((error) => console.log(error));
+      }}
+    >
+      <ComboboxInput
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+        disabled={!ready}
+        placeholder="Enter an address"
+      />
+      <ComboboxPopover>
+        {status === "OK" &&
+          data.map(({ id, description }) => (
+            <ComboboxOption key={id} value={description} />
+          ))}
+      </ComboboxPopover>
+    </Combobox>
+  );
 }
 
 //event OnClick gives you the longitude latitude
