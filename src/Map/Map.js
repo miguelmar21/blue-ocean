05a9ce2cc3ea824a7fb355660d31b5de9a408908
Miguel Regalado <miguelmobil21@gmail.com>
2021-12-10 20:00:02 -0600

    Different icons based on category

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -18,6 +18,7 @@ import {
   ComboboxOption,
 } from "@reach/combobox";
 import mapStyle from "./mapStyle";
+import MarkerForm from "./MarkerForm";
 
 const options = {
   styles: mapStyle,
@@ -30,8 +31,11 @@ const defaultCenter = { lat: 27.522628, lng: -99.489061 };
 const Map = withScriptjs(
   withGoogleMap((props) => {
     const [markers, setMarkers] = useState([]);
+    const [canSetMarker, setCanSetMarker] = useState(true);
     const [selected, setSelected] = useState(null);
     const [panTo, setPanTo] = useState(null);
+    const [category, setCategory] = useState("");
+    const [formDisplayed, setFormDisplayed] = useState("none");
 
     const onMapClick = React.useCallback((event) => {
       setMarkers((currentMarkers) => [
@@ -40,8 +44,11 @@ const Map = withScriptjs(
           lat: event.latLng.lat(),
           lng: event.latLng.lng(),
           time: new Date(),
+          icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Red_dot.svg/2048px-Red_dot.svg.png'
         },
       ]);
+      setCanSetMarker(false);
+      setFormDisplayed('marker-form')
     }, []);
 
     return (
@@ -52,7 +59,7 @@ const Map = withScriptjs(
           defaultZoom={8}
           defaultCenter={defaultCenter}
           options={options}
-          onClick={onMapClick}
+          onClick={canSetMarker && onMapClick}
           ref={(map) => map && panTo !== null && map.panTo(panTo)}
         >
           {markers.map((marker) => (
@@ -60,7 +67,7 @@ const Map = withScriptjs(
               key={marker.time.toISOString()}
               position={{ lat: marker.lat, lng: marker.lng }}
               icon={{
-                url: "https://svg-clipart.com/svg/color/oLsCLwr-blue-musical-note-vector.svg",
+                url: marker.icon,
                 scaledSize: new window.google.maps.Size(30, 30),
                 origin: new window.google.maps.Point(0, 0),
                 anchor: new window.google.maps.Point(15, 15),
@@ -81,6 +88,13 @@ const Map = withScriptjs(
             </InfoWindow>
           ) : null}
         </GoogleMap>
+        <MarkerForm
+          formDisplayed={formDisplayed}
+          setFormDisplayed={setFormDisplayed}
+          setCategory={setCategory}
+          markers={markers}
+          setMarkers={setMarkers}
+        />
       </div>
     );
   })
