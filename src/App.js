e847c6a8ad8da31e127657b9af3d4cb979f295c0
Jeff DeSouza <jeffdesouzadev@gmail.com>
2021-12-13 15:42:31 -0600

    end shared work

--- src/App.js
+++ src/App.js
@@ -5,31 +5,10 @@ import Map from "./Map/Map";
 import PerformersNearby from "./performersNearby/index.js";
 import API_KEY from '../config'
 
-<<<<<<< HEAD
-function App() {
-  const [loggedInUser, setLoggedInUser] = useState();
-
-  return (
-    <div>
-      <Map
-        isMarkerShown
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `400px` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      />
-      {/* <PerformersNearby/> */}
-    </div>
-  );
-}
-=======
-
-
 
 export default function App() {
 const [loggedInUser, setLoggedInUser] = useState();
   return (
->>>>>>> dev
 
       <div>
         <Profiles></Profiles>
