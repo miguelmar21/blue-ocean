e5b85870108a2bf02e2891cf712cf4825bffb083
RasNewton <princegyekyeaddai@gmail.com>
2021-12-13 15:48:45 -0600

    recent dev

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import Profiles from './profiles/profiles.js';
 import Map from "./Map/Map";
 import PerformersNearby from "./performersNearby/index.js";
-import API_KEY from '../config'
+// import API_KEY from '../config';
 
 
 
@@ -16,7 +16,7 @@ const [loggedInUser, setLoggedInUser] = useState();
         <Profiles></Profiles>
         <Map
           isMarkerShown
-          googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${46575}&v=3.exp&libraries=geometry,drawing,places`}
           loadingElement={<div style={{ height: `100%` }} />}
           containerElement={<div style={{ height: `400px` }} />}
           mapElement={<div style={{ height: `100%` }} />}
