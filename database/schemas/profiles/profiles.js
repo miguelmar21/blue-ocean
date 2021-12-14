c7d9dbe7028dfcbc34da2109b68549f0a7667c22
RasNewton <princegyekyeaddai@gmail.com>
2021-12-14 10:03:54 -0600

    passport session authentication complete

--- database/schemas/profiles/profiles.js
+++ database/schemas/profiles/profiles.js
@@ -4,27 +4,27 @@ const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new mongoose.Schema({
   username: {index: true, unique: true, type: String, minLength: 7},
-  // password: String,
-  // is_performer: { type: Boolean, default: false },
-  // name: String,
-  // bio: String,
-  // user_picture: String,
-  // social_media: {twitter: String,
-  //                facebook: String,
-  //                instagram: String,
-  //               },
-  // categories: [String],
-  // favorites: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
-  // band: {name: String,
-  //       members: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]},
-  // media: [String],
-  // performances: [{
-  //   performance_id: {type: Number, unique: true, sparse:true},
-  //   location: String,
-  //   time: String,
-  //   category: String,
-  //   additionalPerformers: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
-  // }]
+  password: String,
+  is_performer: { type: Boolean, default: false },
+  name: String,
+  bio: String,
+  user_picture: String,
+  social_media: {twitter: String,
+                 facebook: String,
+                 instagram: String,
+                },
+  categories: [String],
+  favorites: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+  band: {name: String,
+        members: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]},
+  media: [String],
+  performances: [{
+    performance_id: {type: Number, unique: true, sparse:true},
+    location: String,
+    time: String,
+    category: String,
+    additionalPerformers: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+  }]
 });
 
 userSchema.plugin(passportLocalMongoose);
