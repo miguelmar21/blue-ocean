2db55da7ca75fd9bbdc95eee81406fe9b6fed133
RasNewton <princegyekyeaddai@gmail.com>
2021-12-14 19:27:15 -0600

    edit

--- src/header/headerItems/signout.js
+++ src/header/headerItems/signout.js
@@ -1,20 +1,21 @@
 import React, {useState, useEffect} from 'react';
 import Button from '../button';
+import axios from 'axios';
 
 const Signout = () => {
   const handleClick = () => {
-    axios
-      .get(`http://localhost:3000/signout`, { username: values.username, password: values.password1 })
-      .then(response => {
-        // handleClose();
-        // reset();
-      })
-      .catch(err => {
-        // setErrors({
-        //   ...temp,
-        //   'username': 'Email already has account. Please login.'
-        // })
-      })
+    // axios
+    //   .get(`http://localhost:3000/signout`, { username: values.username, password: values.password1 })
+    //   .then(response => {
+    //     // handleClose();
+    //     // reset();
+    //   })
+    //   .catch(err => {
+    //     // setErrors({
+    //     //   ...temp,
+    //     //   'username': 'Email already has account. Please login.'
+    //     // })
+    //   })
   }
   return(
     <Button
