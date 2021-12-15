4e661bb3f7da9caa5a417540695d1bf6ca2f75b3
Adam Rakla <adamrakla@gmail.com>
2021-12-14 18:24:54 -0600

    favorite button functionality working Profile component complete except favorite List and social media links

--- src/App.js
+++ src/App.js
@@ -11,7 +11,37 @@ import Grid from '@mui/material/Grid';
 
 
 export default function App() {
-const [loggedInUser, setLoggedInUser] = useState();
+
+/* default user
+{user_picture: 'https://northaustinurology.com/app/uploads/2017/01/profile-silhouette.jpg',
+categories: [],
+name: '',
+username: 'Guest',
+band: {name: '', members: []},
+media: []}
+*/
+const [loggedInUser, setLoggedInUser] = useState({
+  username: 'theRealJaden',
+  password: 'password',
+  is_performer: true,
+  name: 'Jaden Smith',
+  bio: "How can mirrors be real if our eyes aren't real?",
+  user_picture: 'https://upload.wikimedia.org/wikipedia/commons/8/88/2019_-_Opening_Night_SM1_0368_%2849015104751%29.jpg',
+  social_media: {
+    twitter: 'https://twitter.com/jaden?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor',
+    facebook: 'https://www.facebook.com/officialjaden',
+    instagram: 'https://www.instagram.com/c.syresmith/'
+  },
+  categories: [ 'Music' ],
+  favorites: null,
+  band: null,
+  media: [
+    'https://www.youtube.com/embed/cmc8q2dcIMs',
+    'https://www.youtube.com/embed/icpl9DEagsk',
+    'https://www.youtube.com/embed/huRFB-urWAc'
+  ],
+  performances: [],
+  });
   return (
       <div>
         <Container>
@@ -19,7 +49,7 @@ const [loggedInUser, setLoggedInUser] = useState();
             <Grid item xs={3}>
               <Grid container>
                 <Grid item xs={12}>
-                  <Profiles></Profiles>
+                  <Profiles updateUser={setLoggedInUser} loggedInUser={loggedInUser}></Profiles>
                 </Grid>
                 <Grid item xs={12}>
                   <PerformersNearby/>
