719c053333e5a990c4b3c5bf3338820af3bca0e4
Adam Rakla <adamrakla@gmail.com>
2021-12-14 18:37:13 -0600

    Removed dummy data from App.js

--- src/App.js
+++ src/App.js
@@ -17,28 +17,7 @@ username: 'Guest',
 band: {name: '', members: []},
 media: []}
 */
-const [loggedInUser, setLoggedInUser] = useState({
-  username: 'theRealJaden',
-  password: 'password',
-  is_performer: true,
-  name: 'Jaden Smith',
-  bio: "How can mirrors be real if our eyes aren't real?",
-  user_picture: 'https://upload.wikimedia.org/wikipedia/commons/8/88/2019_-_Opening_Night_SM1_0368_%2849015104751%29.jpg',
-  social_media: {
-    twitter: 'https://twitter.com/jaden?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor',
-    facebook: 'https://www.facebook.com/officialjaden',
-    instagram: 'https://www.instagram.com/c.syresmith/'
-  },
-  categories: [ 'Music' ],
-  favorites: null,
-  band: null,
-  media: [
-    'https://www.youtube.com/embed/cmc8q2dcIMs',
-    'https://www.youtube.com/embed/icpl9DEagsk',
-    'https://www.youtube.com/embed/huRFB-urWAc'
-  ],
-  performances: [],
-  });
+const [loggedInUser, setLoggedInUser] = useState();
   return (
       <div>
         <Container>
