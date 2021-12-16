a5bac11231123b17bb39b85a068caaf02b066742
Max Zhao <maxzhao98@gmail.com>
2021-12-16 12:09:02 -0600

    cleaned up some unnecessary code that was commented out

--- src/performersNearby/index.js
+++ src/performersNearby/index.js
@@ -11,20 +11,6 @@ function PerformersNearby(props) {
   let [slides, setSlides] = useState([]);
   let location = props.location;
 
-  //miguel will have a state called location that will be eqaul to the current location (lat & long)
-  //usestate location to grab all users from the database where location is <= the search radius
-  //populate slides by mapping through the array of objects sent back from the database
-
-  // let slides = [
-  //   <div className="PN_performer">
-  //     <img src="https://picsum.photos/800/301/?random" alt="2" />
-  //     <div className="centered">Performers Name</div>
-  //   </div>,
-  //   <img src="https://picsum.photos/800/302/?random" alt="3" />,
-  //   <img src="https://picsum.photos/800/303/?random" alt="4" />,
-  //   <img src="https://picsum.photos/800/304/?random" alt="5" />,
-  // ];
-
   const populateSlides = () => {
     axios
       .get("http://localhost:3000/performersNearby", {
@@ -71,14 +57,5 @@ function PerformersNearby(props) {
     </div>
   );
 }
-// return (
-//   <div className="performersNearby">
-//     <div className="PN_filter">
-//       <h1 className="PN header">Performers Nearby</h1>
-//       <SearchRadius setSearchRadius={setSearchRadius} />
-//     </div>
-//     <Carousel slides={slides} />
-//   </div>
-// );
 
 export default PerformersNearby;
