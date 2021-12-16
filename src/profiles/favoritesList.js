09c9f2d7b86b02c2f61efce2104d32445b37c6b3
Adam Rakla <adamrakla@gmail.com>
2021-12-16 08:53:51 -0600

    added getFavorites route(not working)

--- src/profiles/favoritesList.js
+++ src/profiles/favoritesList.js
@@ -1,11 +1,29 @@
-// import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-// export default function FavoritesList(props) {
-//   return(
-//     <div>
-//       <h4>Favorites</h4>
-//       {props.loggedInUser.map((user) => {
-//       })}
-//     </div>
-//   )
-// }
+export default function FavoritesList(props) {
+  const [favorites, setFavorites] = useState();
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:3000/getFavorites', {
+      params: {
+        favorites: props.user.favorites
+      }
+    })
+    .then((favorites) => {
+      console.log(favorites);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  },[props.user]);
+
+  return(
+    <div>
+      {/* <h4>Favorites</h4>
+      {props.loggedInUser.map((user) => {
+      })} */}
+    </div>
+  )
+}
