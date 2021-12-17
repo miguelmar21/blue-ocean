c1e22ce153d826897d31d25cbf17f04d3cee8ad5
Miguel Regalado <miguelmobil21@gmail.com>
2021-12-16 21:49:03 -0600

    Page styling

--- src/header/header.js
+++ src/header/header.js
@@ -1,46 +1,55 @@
-import React, {useEffect, useState} from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Signup from './headerItems/signup';
-import Login from './headerItems/signin';
-import Signout from './headerItems/signout'
-
-
+import React, { useEffect, useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Signup from "./headerItems/signup";
+import Login from "./headerItems/signin";
+import Signout from "./headerItems/signout";
 
 const style = {
-  backgroundColor: '#fff',
-  transform: 'translateZ(0)',
-  elevation: 0
-}
+  backgroundColor: "#fff",
+  transform: "translateZ(0)",
+  elevation: 0,
+};
 
-
-const Header = ({ setLoggedInUser}) => {
-  const [display, setDisplay] = useState('Login');
-  const [username, setUsername] = useState('');
-
+const Header = ({ setLoggedInUser }) => {
+  const [display, setDisplay] = useState("Login");
+  const [username, setUsername] = useState("");
 
   return (
-    <AppBar position="static" sx={style}>
-      <Toolbar>
-        <Grid container alignItems='center'>
-          <Grid item sm={11} style={{}}>
-            <Stack direction="row" spacing={2}>
-              <Login setLoggedInUser={setLoggedInUser} display={display} setDisplay={setDisplay} username={username} setUsername={setUsername}/>
-              <Signup setLoggedInUser={setLoggedInUser} display={display} setDisplay={setDisplay} username={username} setUsername={setUsername}/>
-            </Stack>
+    <div className="header">
+      <AppBar position="static" sx={style}>
+        <Toolbar>
+          <Grid container alignItems="center">
+            <Grid item sm={11} style={{}}>
+              <Stack direction="row" spacing={2}>
+                <Login
+                  setLoggedInUser={setLoggedInUser}
+                  display={display}
+                  setDisplay={setDisplay}
+                  username={username}
+                  setUsername={setUsername}
+                />
+                <Signup
+                  setLoggedInUser={setLoggedInUser}
+                  display={display}
+                  setDisplay={setDisplay}
+                  username={username}
+                  setUsername={setUsername}
+                />
+              </Stack>
+            </Grid>
+            <Grid item sm={1}>
+              <div className="logo"></div>
+            </Grid>
+            <Grid item sm></Grid>
+            <Grid item style={{}}></Grid>
           </Grid>
-          <Grid item sm={1}>
-            <div className="logo"></div>
-          </Grid>
-          <Grid item sm ></Grid>
-          <Grid item style={{}}>
-          </Grid>
-        </Grid>
-      </Toolbar>
-    </AppBar>
-  )
-}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
 
 export default Header;
